9b2fc263bd508899fc072589addf059b7e15666c
Nelson Robles <nelsonroblesv@gmail.com>
2025-12-05 22:01:17 -0600

    Add. User - Model, Migration & Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +19,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Nelson Robles',
+            'email' => 'nelsonroblesv+dev@gmail.com',
+            'password' => Hash::make('iamwao'),
+            'phone' => '9962211095',
+            'birthday' => '1988-03-20'
         ]);
     }
 }
